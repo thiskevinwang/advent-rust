150d27349c7bbd566d5aee68e0bbec455037f345
Kevin Wang <kwangsan@gmail.com>
2022-12-16 22:20:20 -0500

    day part 2

--- src/2022/day1/mod.rs
+++ src/2022/day1/mod.rs
@@ -15,7 +15,7 @@ pub fn solution() {
 
     // println!("{}",res);
 
-    let elves: Vec<u32> = res
+    let mut elves: Vec<u32> = res
         // split by double newlines
         .split("\n\n")
         // ...for each multiline string, call .lines()
@@ -30,7 +30,14 @@ pub fn solution() {
 
     // todo: explain why what .enumerate() does -> that resulted in `// Some((250, 39436))`
     let max_calories = elves.iter().max().unwrap();
+    println!("==> {:?}", max_calories); // 72511
 
-    println!("==> {:?}", max_calories);
-    // 72511
+    // part 2
+    // I needed  to make `elves` mutable so that I could call `sort_by`.
+    elves.sort_by(|a, b| b.cmp(a));
+
+    println!("==> {:?}", elves[0]); // 72511
+
+    // grab a slice (start until index 3) of the elves (list of calories)
+    println!("==> {:?}", &elves[..3].iter().sum::<u32>()); // 212117
 }
