1d9e1ac248f7a9b57d299979c0851581b84c23d8
Kevin Wang <kwangsan@gmail.com>
2022-12-17 08:56:10 -0500

    chore: add template

---
+++ src/2022/template/day{{n}}/mod.rs
@@ -0,0 +1,21 @@
+use rust_embed::RustEmbed;
+use std::str;
+
+// https://crates.io/crates/rust-embed
+#[derive(RustEmbed)]
+#[folder = "src/2022/day{{n}}/"]
+struct Asset;
+pub fn solution() {
+    println!("https://adventofcode.com/2022/day/{{n}}");
+
+    /*
+
+    implement me
+
+    */
+
+    // Part 1
+    println!("==> {}" /* */);
+    // Part 2
+    println!("==> {}" /* */);
+}
