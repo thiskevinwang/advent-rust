d6803be6d8f85cb019c89403feda27922e0863e8
Kevin Wang <kwangsan@gmail.com>
2022-12-17 19:39:36 -0500

    refactor day 4 part 1

--- src/2022/day4/mod.rs
+++ src/2022/day4/mod.rs
@@ -20,22 +20,24 @@ pub fn solution() {
     for pair in assignment_pairs {
         let ranges: Vec<&str> = pair.split(",").collect();
         if let [l, r] = ranges[..] {
-            let l_bounds = l.split("-").collect::<Vec<&str>>();
-            let r_bounds = r.split("-").collect::<Vec<&str>>();
+            let l_bounds: Vec<u32> = l.split("-").map(|e| e.parse::<u32>().unwrap()).collect();
+            let r_bounds: Vec<u32> = r.split("-").map(|e| e.parse::<u32>().unwrap()).collect();
 
-            let l_min = l_bounds[0].parse::<u32>().unwrap();
-            let l_max = l_bounds[1].parse::<u32>().unwrap();
-            let r_min = r_bounds[0].parse::<u32>().unwrap();
-            let r_max = r_bounds[1].parse::<u32>().unwrap();
+            let l_min = l_bounds[0];
+            let l_max = l_bounds[1];
+            let r_min = r_bounds[0];
+            let r_max = r_bounds[1];
 
-            if (l_min >= r_min && l_max <= r_max) || (r_min >= l_min && r_max <= l_max) {
+            let l_in_r = l_min >= r_min && l_max <= r_max;
+            let r_in_l = r_min >= l_min && r_max <= l_max;
+            if l_in_r || r_in_l {
                 overlap_ct += 1;
             }
         }
     }
 
     // Part 1
-    println!("==> {}", overlap_ct);
+    println!("==> {}", overlap_ct); // 567
 
     // Part 2
     // println!("==> {}" /* */);
